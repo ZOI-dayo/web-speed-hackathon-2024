1a2c082dc7e35a59443400e5a94f978fdf42e675
ZOI-dayo <zoizoi.dayo@gmail.com>
2024-09-22 04:04:45 +0900

    N+1

--- workspaces/app/src/features/book/components/BookListItem.tsx
+++ workspaces/app/src/features/book/components/BookListItem.tsx
@@ -9,7 +9,7 @@ import { Spacer } from '../../../foundation/components/Spacer';
 import { Text } from '../../../foundation/components/Text';
 import { useImage } from '../../../foundation/hooks/useImage';
 import { Color, Radius, Space, Typography } from '../../../foundation/styles/variables';
-import { useBook } from '../hooks/useBook';
+import { useBookList } from '../hooks/useBookList';
 
 const _Wrapper = styled.li`
   width: 100%;
@@ -32,7 +32,8 @@ type Props = {
 };
 
 export const BookListItem: React.FC<Props> = ({ bookId }) => {
-  const { data: book } = useBook({ params: { bookId } });
+  const { data: bookList } = useBookList();
+  const book = bookList.find((book) => book.id === bookId);
 
   const imageUrl = useImage({ height: 64, imageId: book.image.id, width: 64 });
 
